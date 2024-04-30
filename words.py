dd3aaa47eea3a70ff266f2afd9e3b61d5a28a807
Urte Vycinaite <urte.vycinaite@gmail.com>
2024-04-30 19:49:23 +0300

    third commit

---
+++ words.py
@@ -0,0 +1,27 @@
+import random
+
+
+def get_random_word():
+    words = [
+        "umbrella",
+        "computer",
+        "television",
+        "python",
+        "watermelon",
+        "sunglasses",
+        "rainbow",
+        "helicopter",
+        "flashlight",
+        "hangman",
+        "airplane",
+        "handbag",
+        "perfume",
+        "mascara",
+        "cherry",
+        "strawberry",
+        "blueberry",
+        "fridge",
+        "elephant",
+        "mouse",
+    ]
+    return random.choice(words)
